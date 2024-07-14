97da1d29846047106661d8692aa492e904ae4c6d
Mohammed Yunus Shaikh <mohammedyunusshaikh@Mohammeds-MacBook-Air.local>
2024-07-14 21:14:18 +0530

    Change dir of clerk and stripe

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -8,14 +8,12 @@ import { NextResponse } from 'next/server'
  
 export async function POST(req: Request) {
  
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
  
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
  
-  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
   const svix_timestamp = headerPayload.get("svix-timestamp");
@@ -24,23 +22,19 @@ export async function POST(req: Request) {
   console.log("Received headers:", { svix_id, svix_timestamp, svix_signature });
 
  
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
     })
   }
  
-  // Get the body
   const payload = await req.json()
   const body = JSON.stringify(payload);
  
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
  
   let evt: WebhookEvent
  
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -54,7 +48,6 @@ export async function POST(req: Request) {
     })
   }
  
-  // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
  
